3e96eabcfc6eb340fddc52990dbccb004d379534
Luke <inyong.ham@gmail.com>
2026-06-30 09:33:10 +0900

    feat(infra): terraform Aurora PostgreSQL Serverless v2 (pgvector) for AWS MVP

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,2 +1,3 @@
 output "bucket_name"                 { value = aws_s3_bucket.data.bucket }
 output "bedrock_s3_instance_profile" { value = aws_iam_instance_profile.app.name }
+output "aurora_endpoint"             { value = aws_rds_cluster.aurora.endpoint }
