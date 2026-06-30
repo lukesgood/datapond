349c97ff73e3d44c12b386fe08984bf04d5d6e6d
Luke <inyong.ham@gmail.com>
2026-06-30 15:49:35 +0900

    feat(infra): optional IRSA role for LiteLLM Bedrock access on EKS

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "bucket_name"                 { value = aws_s3_bucket.data.bucket }
 output "bedrock_s3_instance_profile" { value = aws_iam_instance_profile.app.name }
 output "aurora_endpoint"             { value = aws_rds_cluster.aurora.endpoint }
+output "litellm_bedrock_role_arn"    { value = local.irsa_enabled ? aws_iam_role.litellm_bedrock[0].arn : "" }
